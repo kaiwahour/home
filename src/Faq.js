f467181158b9241c1e54851cd3018653e821f68a
Alan Wong <captainalan@gmail.com>
2019-05-11 11:40:24 -0700

    Adding some FAQs

--- src/Faq.js
+++ src/Faq.js
@@ -1,9 +1,31 @@
 import React from 'react';
+import './Faq.css';
 
 const Faq = () =>
-  <div className="App-content">
-    <h2>Frequently Asked Questions (FAQ)</h2>
-    <p>So you wanna know more about Kaiwa Hour...</p>
+  <div className="Faq">
+    <div className="App-content">
+      <h2>Frequently Asked Questions (FAQ)</h2>
+
+      <div className="Question">
+        <h3>Are non-students welcome?</h3>
+        <p>
+          Yes! While the majority of attendees of Kaiwa Hour are UC Davis students or
+          otherwise affiliated with UC Davis, non-students (including alumni, out of 
+          town visitors, etc.) are welcome.
+        </p>
+      </div>
+
+      <div className="Question">
+        <h3>Do I need to already speak Japanese to attend?</h3>
+        <p>
+          More Japanese will certainly make your experience richer. However, 
+          we don't restrict attendance based on skill level. Be prepared to 
+          dive in and learn fast!
+        </p>
+      </div>
+
+
+    </div>
   </div>
 
 export default Faq;

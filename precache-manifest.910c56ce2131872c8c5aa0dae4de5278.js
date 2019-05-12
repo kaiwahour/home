e9e9530ed3ccb1048143edd437297fc393df69a0
Alan Wong <captainalan@gmail.com>
2019-05-11 17:13:37 -0700

    Updates

---
+++ precache-manifest.910c56ce2131872c8c5aa0dae4de5278.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2786b8e7773132b0fc4db2bf17444a9f",
+    "url": "/home/index.html"
+  },
+  {
+    "revision": "8f00c82200d6bea06563",
+    "url": "/home/static/css/main.c348ce5d.chunk.css"
+  },
+  {
+    "revision": "fe086f643b1ac4d2a25e",
+    "url": "/home/static/js/2.dcb0b9e9.chunk.js"
+  },
+  {
+    "revision": "8f00c82200d6bea06563",
+    "url": "/home/static/js/main.5211e93e.chunk.js"
+  },
+  {
+    "revision": "4e1ec8947a6857600002",
+    "url": "/home/static/js/runtime~main.0990ee84.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/home/precache-manifest.ecc7712c84f5fef50cd29ce231bfd976.js"
+  "/home/precache-manifest.910c56ce2131872c8c5aa0dae4de5278.js"
 );
 
 self.addEventListener('message', (event) => {

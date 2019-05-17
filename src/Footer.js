5c57a04ca7fff3a7d17df6b74e9161662e0f53ab
Alan Wong <captainalan@gmail.com>
2019-05-17 11:43:15 -0700

    fixing some design things

--- src/Footer.js
+++ src/Footer.js
@@ -7,23 +7,29 @@ import { FacebookProvider, Like } from 'react-facebook';
 const Footer = () =>
   <div id="footer">
     <div id="socialmedia" className="footerBlock">
-      <p>FIND US ON SOCIAL MEDIA</p>
+      <h4>Find Us on Social Media</h4>
       <TwitterFollowButton screenName={'kaiwahour'}/>
       <FacebookProvider appId="348662969124728"> {/* oh boy public app id */}
         <Like href="http://www.facebook.com/kaiwahour" colorScheme="dark" showFaces share /> 
       </FacebookProvider>
+      {/* Other links here */}
       <ul>
-        <a  href="https://discord.gg/fra3Vym"><img src="https://discordapp.com/assets/e4923594e694a21542a489471ecffa50.svg"/></a>
+        <a href="https://discord.gg/fra3Vym">
+          <img 
+            id="discord_logo" src="https://discordapp.com/assets/94db9c3c1eba8a38a1fcf4f223294185.png"
+            height="40px"
+          />
+         </a>
       </ul>
     </div>
 
     <div id="sponsors-and-affiliates" className="footerBlock">
-      <p>SPONSORS AND AFFILIATES</p>
+      <h4>Sponsors and Affiliates</h4>
       <p>We are proudly independent! (For now...)</p>
     </div>
 
     <div id="language-switcher" className="footerBlock">
-      <p>THIS PAGE IN OTHER LANGUAGES</p>
+      <h4>This Page in Other Languages</h4>
       <ul>
         <li><Link className="language" to="/translating?lang=jp">日本語</Link></li>
         <li><Link className="language" to="/translating?lang=zh">中文</Link></li>
@@ -32,8 +38,7 @@ const Footer = () =>
       </ul>
     </div>
     <div id="legal-stuff" className="footerBlock">
-      <p>LAST UPDATED MAY 2019</p>
-      <p>COPYRIGHT 2019</p>
+      <p>Last Updated May 2019 — Copyright 2019</p>
     </div>
   </div>
 

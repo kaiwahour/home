2605ff1b653195e665426b1f691480e9f9b6376d
Alan Wong <captainalan@gmail.com>
2019-05-17 20:49:33 -0700

    tuning some styles

--- src/Home.js
+++ src/Home.js
@@ -9,10 +9,10 @@ export default class Home extends Component{
       /* Settings for carousel */
       let settings = {
           dots: false,
-          infinite: false,
+          infinite: true,
           speed: 500,
           slidesToShow: 5, /* I like fibonacci sequence */
-          slidesToScroll: 5,
+          slidesToScroll: 1,
           initialSlide: 0,
           responsive: [
               {
@@ -46,42 +46,42 @@ export default class Home extends Component{
 
             <Slider {...settings} id="Slider">
               <div>
-                <h3>Hot Pot</h3>
+                <h3 className="slider-caption">Hot Pot</h3>
                 <img className="image"
                   src="https://scontent-sjc3-1.xx.fbcdn.net/v/t1.0-9/53295624_2333580953588710_8542725741706477568_o.jpg?_nc_cat=109&_nc_ht=scontent-sjc3-1.xx&oh=3301eb267921a617f649fb4dbce907cc&oe=5D6D402E"
                   alt="Hot Pot"
                 />
               </div>
               <div>
-                <h3><i>Izakaya</i></h3>
+                <h3 className="slider-caption">Izakaya</h3>
                 <img className="image"
                   src="https://scontent-lax3-1.xx.fbcdn.net/v/t1.0-9/42045153_2227417317538408_5089713387391156224_o.jpg?_nc_cat=110&_nc_ht=scontent-lax3-1.xx&oh=3caf39b74bb331d20809d34d281e39e3&oe=5D624689"
                   alt="Izakaya"
                 />
               </div>
               <div>
-                <h3>Foodsies</h3>
+                <h3 className="slider-caption">Foodsies</h3>
                 <img className="image"
                   src="https://scontent-lax3-1.xx.fbcdn.net/v/t1.0-9/41971929_2227417984205008_5384978129699733504_o.jpg?_nc_cat=101&_nc_ht=scontent-lax3-1.xx&oh=60fcb59fe6991940246f0bee595a6fa3&oe=5D5841F3"
                   alt="Foodsies"
                 />
               </div>
               <div>
-                <h3>House Party</h3>
+                <h3 className="slider-caption">House Party</h3>
                 <img className="image"
                   src="https://scontent-lax3-1.xx.fbcdn.net/v/t1.0-9/41934075_2227419604204846_5714010948566515712_o.jpg?_nc_cat=103&_nc_ht=scontent-lax3-1.xx&oh=462f2215180c6c98c6844dc5dc9a3efb&oe=5D5417D3"
                   alt="House Party"
                 />
               </div>
               <div>
-                <h3>At The Gym</h3>
+                <h3 className="slider-caption">At The Gym</h3>
                 <img className="image"
                   src="https://scontent-lax3-1.xx.fbcdn.net/v/t1.0-9/41954801_2227514500862023_1753684431706521600_n.jpg?_nc_cat=111&_nc_ht=scontent-lax3-1.xx&oh=f83783614b486224dfb8cbb89aef4be8&oe=5D584815"
                   alt="At the Gym"
                 />
               </div>
               <div>
-                <h3>Safety First</h3>
+                <h3 className="slider-caption">Safety First</h3>
                 <img className="image"
                   src="https://scontent-lax3-1.xx.fbcdn.net/v/t1.0-9/16142195_1867299683550175_6997641311702090329_n.png?_nc_cat=107&_nc_ht=scontent-lax3-1.xx&oh=0ab931ad684fba29ac7fa9032baf644e&oe=5D629F52"
                   alt="How to sits"

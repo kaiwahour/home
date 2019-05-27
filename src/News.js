bc098529c4da432db34b9383b80029ebc7992b4b
Alan Wong <captainalan@gmail.com>
2019-05-26 18:51:22 -0700

    News stories are new fetched json rather than hard coded javascript

--- src/News.js
+++ src/News.js
@@ -1,33 +1,6 @@
 import React, {Component} from 'react';
+import axios from 'axios';
 import './News.css'
-
-/* Temporary fake data */
-let stories = [
-  {
-    id: 1,
-    date: "05/22/2019",
-    en: {
-      title: "Website development goes well!",
-      body: "We are making a thing and it is looking good."
-    },
-    jp: {
-      title: "サイト発達よくやる",
-      body: "このサイトやつ…かっこいいね"
-    }
-  },
-  {
-    id: 2,
-    date: "05/22/2019",
-    en: {
-      title: "Meetings continue",
-      body: "You know the drill; Tuesdays from 5-7 PM in Olson 53a."
-    },
-    jp: {
-      title: "ミーティング",
-      body: "計画通り　ミーティングが続く。"
-    }
-  }
-]
 
 class News extends Component {
 
@@ -35,14 +8,22 @@ class News extends Component {
     super(props);
     this.state = {
       component_language: 'en',   // English by default for now
-      stories: stories, // This should be fetched in componentDidMount() with real data
+      stories: [], // This should be fetched in componentDidMount() with real data
     }
 
     this.toggleLanguage = this.toggleLanguage.bind(this);
   }
 
   componentDidMount() {
-
+    axios.get("/fakeApi/fakeNews.json")
+      .then(response => {
+        const stories = response.data;
+        console.log(stories);
+        this.setState({ stories });
+      })
+      .catch(error => {
+        console.log("Something went wrong.", error);
+      });
   }
 
   toggleLanguage(props){
